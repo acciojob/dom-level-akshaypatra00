285e1d85f07b981a772e3a6aac79fe7bdaa964c4
Akshay patra <akshaykumarpatra00@gmail.com>
2026-05-25 12:54:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let ele = getelementbyId("level")
+let level = 0
+while (ele.parentElement) {
+	level+=ele.parentElement
+}
+alert(~the level of the element is:${level}~)
